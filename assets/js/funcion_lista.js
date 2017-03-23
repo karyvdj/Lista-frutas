13267ac6f3b43c8e1eb74728a5f5a790dfbc4f00
OfeliaGarc <ofee.50.og@gmail.com>
2017-03-23 09:45:57 -0600

    añade y quita las frutas :)

--- assets/js/funcion_lista.js
+++ assets/js/funcion_lista.js
@@ -1,8 +1,14 @@
-var enLista = [];
 function listaFruta(){
   var añadirFruta = document.getElementById('agregarFruta').value;
-  if (añadirFruta != ""){
-    document.getElementById("listaOrdenada").innerHTML += "<li>" + añadirFruta + "</li>";
-    enLista.push(añadirFruta);
+  if(añadirFruta != ""){
+    var nuevaFruta = document.createElement("li");
+    nuevaFruta.innerHTML= añadirFruta;
+    document.getElementById('listaOrdenada').appendChild(nuevaFruta);
+    document.getElementById('agregarFruta').value="";
   }
+}
+function quitarFruta(){
+  var frutaQuitar= document.getElementById('listaOrdenada');
+  console.log(frutaQuitar);
+  frutaQuitar.removeChild(frutaQuitar.lastElementChild);
 }
